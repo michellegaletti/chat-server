1366c37b163b277b504e49f2121976b2c6231fac
michellegaletti <msgaletti@gmail.com>
2021-04-27 12:47:58 -0600

    Completing chat-server lab

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mc = require("./controllers/messages_controller");
+
+const app = express();
+
+app.use(express.json());
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+app.use(express.static(__dirname + "/../public/build"));
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
